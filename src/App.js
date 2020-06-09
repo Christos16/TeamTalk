487f0a435d9438423f3673fbd9a41803f9cbf817
Christos Malamas <christosmalamas@outlook.com>
2020-06-09 13:52:44 +0300

    last commit before deployment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Join from './components/Join/Join';
+import Main from './components/Main/Main';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Router>
+    <Route path='/' exact component={Join} />
+    <Route path='/main' exact component={Main} />
+  </Router>
+);
 
 export default App;
